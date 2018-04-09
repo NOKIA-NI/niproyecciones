1d2498a1e90950499d27deeeeb7b2fc3e8c82e40
Julio Brito <jucebridu@gmail.com>
2018-04-08 22:25:48 -0500

    se agregan url search

--- static/dashboard/js/dashboard.js
+++ static/dashboard/js/dashboard.js
@@ -8,10 +8,22 @@ function search_url (form) {
   var current_url = window.location.href
   var url_search_estacion = '/estaciones/search/estacion/'
   var url_search_parte = '/partes/search/parte/'
+  var url_search_proyeccion = '/proyecciones/search/proyeccion/'
+  var url_search_hw_actividad = '/hw_actividades/search/hw/actividad/'
+  var url_search_consumo_nokia = '/consumos/search/consumo/nokia/'
+  var url_search_consumo_claro= '/consumos/search/consumo/claro/'
   if (current_url.includes('/estaciones/')) {
     form.action = url_search_estacion;
   } else if (current_url.includes('/partes/')) {
     form.action = url_search_parte;
+  } else if (current_url.includes('/proyecciones/')) {
+    form.action = url_search_proyeccion;
+  } else if (current_url.includes('/hw/actividad/')) {
+    form.action = url_search_hw_actividad;
+  } else if (current_url.includes('/consumo/nokia/')) {
+    form.action = url_search_consumo_nokia;
+  } else if (current_url.includes('/consumo/claro/')) {
+    form.action = url_search_consumo_claro;
   } else {
     form.action = ''
   }
