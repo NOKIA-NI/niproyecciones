99f005c99a4257383c49f95ad81b6c79b7003f5f
Julio Brito <jucebridu@gmail.com>
2018-04-18 21:03:54 -0500

    se agregan url search para llegada, existencia y resultado

--- static/dashboard/js/dashboard.js
+++ static/dashboard/js/dashboard.js
@@ -12,6 +12,10 @@ function search_url (form) {
   var url_search_hw_actividad = '/hw_actividades/search/hw/actividad/'
   var url_search_consumo_nokia = '/consumos/search/consumo/nokia/'
   var url_search_consumo_claro= '/consumos/search/consumo/claro/'
+  var url_search_llegada= '/llegadas/search/llegada/'
+  var url_search_existencia= '/existencias/search/existencia/'
+  var url_search_resultado= '/resultados/search/resultado/'
+
   if (current_url.includes('/estaciones/')) {
     form.action = url_search_estacion;
   } else if (current_url.includes('/partes/')) {
@@ -24,6 +28,12 @@ function search_url (form) {
     form.action = url_search_consumo_nokia;
   } else if (current_url.includes('/consumo/claro/')) {
     form.action = url_search_consumo_claro;
+  } else if (current_url.includes('/llegada/')) {
+    form.action = url_search_llegada;
+  } else if (current_url.includes('/existencia/')) {
+    form.action = url_search_existencia;
+  } else if (current_url.includes('/resultado/')) {
+    form.action = url_search_resultado;
   } else {
     form.action = ''
   }
