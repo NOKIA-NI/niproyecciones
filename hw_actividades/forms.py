ab62aded0d5a95fd6791587cb33b34b5ee51d71e
Julio Brito <jucebridu@gmail.com>
2018-05-16 07:38:38 -0500

    se implementan filtros por relacion en hwActividad

--- hw_actividades/forms.py
+++ hw_actividades/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from .models import HwActividad
+from estaciones import choices as estaciones_choices
+from partes import choices as partes_choices
 
 class HwActividadForm(ModelForm):
     
@@ -9,6 +11,13 @@ class HwActividadForm(ModelForm):
         fields = '__all__'
 
 class FilterHwActividadForm(ModelForm):
+    region = forms.ChoiceField(choices=estaciones_choices.REGION_CHOICES, required=False)
+    bolsa = forms.ChoiceField(choices=estaciones_choices.BOLSA_CHOICES, required=False)
+    comunidades = forms.ChoiceField(choices=estaciones_choices.COMUNIDADES_CHOICES, required=False)
+    satelital = forms.ChoiceField(choices=estaciones_choices.SATELITAL_CHOICES, required=False)
+    w_fc_imp = forms.IntegerField(required=False)
+    w_fc_sal = forms.IntegerField(required=False)
+    grupo_parte = forms.ChoiceField(choices=partes_choices.GRUPO_PARTE_CHOICES, required=False)
 
     class Meta:
         model = HwActividad
