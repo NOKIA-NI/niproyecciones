dba84e275cd1dcf601708264d17855a264f12e33
Julio Brito <jucebridu@gmail.com>
2018-05-29 17:28:19 -0500

    feature_consultas en progreso

--- consultas/models.py
+++ consultas/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.urls import reverse
 from . import choices
+from django.db import connection
+from django.core import serializers
+import json
 
 class Consulta(models.Model):
     nombre = models.CharField(max_length=255)
     descripcion = models.TextField()
     contenido = models.TextField()
     tipo_consulta = models.CharField(max_length=255, choices=choices.TIPO_CONSULTA_CHOICES, blank=True, null=True)
-    data = JSONField()
+    data = JSONField(blank=True, null=True)
 
     estado = models.BooleanField(default=True, editable=False)
     subestado = models.BooleanField(default=False, editable=False)
@@ -25,3 +28,15 @@ class Consulta(models.Model):
 
     #  def get_absolute_url(self):
     #     return reverse('consultas:detail_consulta', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        if self.contenido is not None:
+            with connection.cursor() as cursor:
+                cursor.execute(self.contenido)
+                # data = cursor.fetchall()
+                columns = [col[0] for col in cursor.description]
+                data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+                print(data)
+            self.data = data
+            # self.data = json.dumps(data)
+        super(Consulta, self).save(*args, **kwargs)
--- consultas/urls.py
+++ consultas/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import (
-ConsultaView,
+ListConsulta,
+DetailConsulta,
+CreateConsulta,
 )
 
 app_name = 'consultas'
 
 urlpatterns = [
-    path('list/consulta', ConsultaView.as_view(), name='list_consulta'),
+    path('list/consulta', ListConsulta.as_view(), name='list_consulta'),
+    path('detail/consulta', DetailConsulta.as_view(), name='detail_consulta'),
+    path('create/consulta', CreateConsulta.as_view(), name='create_consulta'),
 ]
